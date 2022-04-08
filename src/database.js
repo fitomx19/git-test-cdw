f5b6114f29d60c4ff81b9b786bfb994d7ec5c3cb
fitomx19 <fitox.metallica@gmail.com>
2022-04-08 11:51:02 -0500

    first commit

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 //sudo service mongod start
 export  async function connect(){
  try {
-    await  mongoose.connect('mongodb://localhost/mongodbgrapghql',{
+    await  mongoose.connect('mongodb+srv://root:kinect123@cluster0.r5zne.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
